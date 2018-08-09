bc9971277cb5244bb369b22e1e8ed0f94b69f4db
leahchoi <soora913@gmail.com>
2018-08-09 15:40:30 -0700

    adds item onto the todolist

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,44 @@
 import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
+import React, {Component} from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
 import TodoList from './todo_list';
+import AddItem from './add_item';
+import listData from '../data/todo'
 
-const App = () => (
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            items: []
+        };
+    }
+    componentDidMount(){
+        this.getListData();
+    }
+
+    getListData(){
+        this.setState({
+            items:listData
+        });
+    }
+    addItem(item){
+        item._id = new Date().getTime();
+        this.setState({
+            items: [item, ...this.state.items]
+        })
+    }
+    render(){
+        console.log('App state: ', this.state)
+    return(
     <div className = "container">
-        <h1 className='center'>To Do List</h1>        
-        <TodoList/>
+        <h1 className='center'>To Do List</h1> 
+        <AddItem add= {this.addItem.bind(this)}/>
+        <TodoList list={this.state.items}/>
     </div>
-);
+    );
+}
+}
 
 export default App;
