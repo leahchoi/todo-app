65096b207d4ef3c4703da86235efae9f317e94a1
leahchoi <soora913@gmail.com>
2018-08-09 18:21:39 -0700

    added axios

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,11 @@ import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
 import TodoList from './todo_list';
 import AddItem from './add_item';
-import listData from '../data/todo'
+import listData from '../data/todo';
+import axios from 'axios';
+
+const BASE_URL = 'http://api.reactprototypes.com';
+const API_KEY = '?key=lfz_data_soora'
 
 class App extends Component {
     constructor(props){
@@ -18,16 +22,25 @@ class App extends Component {
         this.getListData();
     }
 
-    getListData(){
+    // getListData(){
+    //     const resp = axios.get(`${BASE_URL}/todos${API_KEY}`).then((resp)=> {
+    //         this.setState({
+    //             items: resp.data.todos
+    //         });
+    //     }).catch((err)=> {
+    //         console.log('There was an error: ', err.message);
+    //     });
+
+    // }
+    async getListData(){
+        const resp = await axios.get(`${BASE_URL}/todos${API_KEY}`);
         this.setState({
-            items:listData
+            items: resp.data.todos
         });
     }
-    addItem(item){
-        item._id = new Date().getTime();
-        this.setState({
-            items: [item, ...this.state.items]
-        })
+    async addItem(item){
+        const resp = await axios.post(`${BASE_URL}/todos${API_KEY}`, item);
+        this.getListData()
     }
     render(){
         console.log('App state: ', this.state)
